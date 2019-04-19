cff5cc1283646afd997a0011e40ed6850475849c
Aldo Ziflaj <aldoziflaj95@gmail.com>
2019-04-19 19:15:36 +0200

    CRUD topics

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -16,9 +16,7 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
-    get "/", TopicController, :index
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
+    resources "/", TopicController
   end
 
   # Other scopes may use custom stacks.
